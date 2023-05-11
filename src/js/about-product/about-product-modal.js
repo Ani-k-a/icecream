18b86fb2d3844b04c3b8b1cf0c2ee3e0625ecf8d
Hanna.Lytvynenko <ani-k-a@ukr.net>
2023-05-11 22:33:24 +0200

    modal

--- src/js/about-product/about-product-modal.js
+++ src/js/about-product/about-product-modal.js
@@ -2,7 +2,7 @@
 console.log('Does it work?');
 
 import { state } from './about-product-modal-state';
-import { renderItemNutrVal } from './render-item-nutritional-value';
+import { renderItemNutrItem } from './render-item-nutritional-item';
 import { renderIgngidientItem } from './render-igngidient-item';
 import { renderForm } from '../renderForm';
 
@@ -12,13 +12,13 @@ const refs = {
   greenBtn: document.getElementById('button_green'),
   yellowBtn: document.getElementById('button_yellow'),
   orangeBtn: document.getElementById('button_orange'),
-  body:     document.querySelector('body')
+  body: document.querySelector('body')
 };
 
 const renderNutritionalValue = (arr) => {
   const ulNutrVal = document.createElement('ul');
   ulNutrVal.classList.add('ulNutrVal');
-  ulNutrVal.append(...arr.map(el => renderItemNutrVal(el)));
+  ulNutrVal.append(...arr.map(el => renderItemNutrItem(el)));
   return ulNutrVal;
 };
 
@@ -29,7 +29,7 @@ const renderIngredientsList = (arr) => {
   return ulIngrList;
 };
 
-const renderModal = ({name, src, arr, arr2}) => {
+const renderModal = ({ name, src, arr, arr2 }) => {
   const backdrop = document.createElement('div');
   backdrop.classList.add('backdrop');
 
@@ -37,7 +37,10 @@ const renderModal = ({name, src, arr, arr2}) => {
   modal.classList.add('modal');
 
   const block = document.createElement('div');
-  block.classList.add('block');
+  block.classList.add('modal-block');
+
+  const closeBtn = document.createElement('button');
+  closeBtn.setAttribute('class', 'btn-close fa-solid fa-xmark')
 
   const h2 = document.createElement('h2');
   h2.classList.add('modal__title');
@@ -64,7 +67,7 @@ const renderModal = ({name, src, arr, arr2}) => {
 
   divIngredient.append(h3, renderIngredientsList(arr2));
 
-  block.append(h2, picContainer, renderNutritionalValue(arr), divIngredient, renderForm());
+  block.append(h2, closeBtn, picContainer, renderNutritionalValue(arr), divIngredient, renderForm());
 
   modal.append(block);
 
@@ -73,12 +76,20 @@ const renderModal = ({name, src, arr, arr2}) => {
   return backdrop;
 };
 
-function onClick(state) {
-    console.log('Does it work?');
-    renderModal(state);
+
+const onGreenBtnClick = () => {
+  refs.body.append(renderModal(state[0]));
+}
+
+const onYellowBtnClick = () => {
+  refs.body.append(renderModal(state[1]));
+}
+
+const onOrangeBtnClick = () => {
+  refs.body.append(renderModal(state[1]));
 }
 
 
-refs.greenBtn.addEventListener('click', onClick(state[0]));
-refs.yellowBtn.addEventListener('click', onClick(state[1]));
-refs.orangeBtn.addEventListener('click', onClick(state[2]));
+refs.greenBtn.addEventListener('click', onGreenBtnClick);
+refs.yellowBtn.addEventListener('click', onYellowBtnClick);
+refs.orangeBtn.addEventListener('click', onOrangeBtnClick);
